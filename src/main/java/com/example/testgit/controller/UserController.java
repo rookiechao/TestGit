0566b513882a4746c333d202ba2643850ead589e
wuchao <1073647471@qq.com>
2025-07-11 00:32:28 +0800

    add getGender

--- src/main/java/com/example/testgit/controller/UserController.java
+++ src/main/java/com/example/testgit/controller/UserController.java
@@ -2,4 +2,8 @@ package com.example.testgit.controller;
 
 public class UserController {
     private String age;
+    private String gender;
+    private String getGender(){
+        return  gender;
+    }
 }
